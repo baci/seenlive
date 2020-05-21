30012488b18fa0535f8b27ced3b0b76fe0239f40
Till Riemer <till.riemer@gmail.com>
2020-05-21 19:20:04 +0200

    added ASP.NET Core server stubs

---
+++ seenlive-server/SeenLive.Server/SeenLive.Server/Controllers/BandController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeenLive.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BandController : ControllerBase
+    {
+
+    }
+}
