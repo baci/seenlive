cb14946b045e3d5586c1ea6c42615ff3fa1ca3d5
Till Riemer <till.riemer@gmail.com>
2020-07-01 16:29:22 +0200

    configured server with support for swagger and routing

--- seenlive-server/SeenLive.Server/Controllers/BandController.cs
+++ seenlive-server/SeenLive.Server/Controllers/BandController.cs
@@ -1,27 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SeenLive.Server.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SeenLive.Server.Controllers
-{
-    [Route("api/[controller]")]
+{  
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class BandController : ControllerBase
     {
+        private IEnumerable<ConcertEntryDTO> _concertEntries;
+
         [HttpPost]
         public IActionResult AddConcertEntry(ConcertEntryDTO concertEntry)
         {
-            // TODO: vorerst Objekt in lokalem File als JSON speichern
+            // TODO: save as JSON object to local file for now
 
-            return NotFound();
+            _concertEntries ??= new List<ConcertEntryDTO>();
+
+            _concertEntries.ToList().Add(concertEntry);
+
+            return Ok();
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<ConcertEntryDTO>> GetConcertEntries()
         {
-            // TODO: vorerst Objekte aus lokalem File laden
-
-            return NotFound();
+            return Ok(_concertEntries);
         }
     }
 }
