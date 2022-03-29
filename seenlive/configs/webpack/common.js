d82a7e2f1c9ac9500c0da3a120547618481e2203
TipluTillR <t.riemer@tiplu.de>
2022-03-29 12:03:29 +0200

    webpack common.js upgrade fix

--- seenlive/configs/webpack/common.js
+++ seenlive/configs/webpack/common.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.(scss|sass)$/,
-        loaders: [
+        rules: [
           'style-loader',
           { loader: 'css-loader', options: { importLoaders: 1 } },
           'sass-loader',
@@ -33,7 +33,7 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
+        rules: [
           'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
           'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
         ],
