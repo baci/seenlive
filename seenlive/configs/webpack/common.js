a305ad4925febacc3e1458070b2ab2db2c59e348
TipluTillR <t.riemer@tiplu.de>
2022-03-29 12:10:17 +0200

    more fixes

--- seenlive/configs/webpack/common.js
+++ seenlive/configs/webpack/common.js
@@ -26,16 +26,16 @@ module.exports = {
       {
         test: /\.(scss|sass)$/,
         rules: [
-          'style-loader',
+		  {loader: 'style-loader'},
           { loader: 'css-loader', options: { importLoaders: 1 } },
-          'sass-loader',
+		  {'sass-loader'},
         ],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         rules: [
-          'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+		{loader: 'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]'},
+			{loader: 'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false'},
         ],
       },
     ],
