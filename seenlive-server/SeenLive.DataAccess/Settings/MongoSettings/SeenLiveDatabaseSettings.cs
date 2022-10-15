fdbcddc6be04f1ee39077b18f7dd22ed42316db4
Till Riemer <till.riemer@gmail.com>
2022-10-15 22:32:47 +0200

    server: feeding environment variables from docker-compose host

--- seenlive-server/SeenLive.DataAccess/Settings/MongoSettings/SeenLiveDatabaseSettings.cs
+++ seenlive-server/SeenLive.DataAccess/Settings/MongoSettings/SeenLiveDatabaseSettings.cs
@@ -5,9 +5,9 @@ namespace SeenLive.DataAccess.Settings.MongoSettings
 {
     public class SeenLiveDatabaseSettings : ISeenLiveDatabaseSettings
     {
-        private readonly string username = Environment.GetEnvironmentVariable("seenlive-db-username", EnvironmentVariableTarget.User);
-        private readonly string password = Environment.GetEnvironmentVariable("seenlive-db-password", EnvironmentVariableTarget.User);
-        private readonly string server = Environment.GetEnvironmentVariable("seenlive-db-server", EnvironmentVariableTarget.User);
+        private readonly string username = Environment.GetEnvironmentVariable("seenlive-db-username");
+        private readonly string password = Environment.GetEnvironmentVariable("seenlive-db-password");
+        private readonly string server = Environment.GetEnvironmentVariable("seenlive-db-server");
 
         public string ArtistsCollectionName { get; set; }
         public string DatesCollectionName { get; set; }
